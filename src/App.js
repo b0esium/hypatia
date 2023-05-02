c28c4c119f79dc02db5578f187651e924c575534
b0esium <b0esium@protonmail.com>
2023-05-02 12:02:59 +0200

    Page layout with fixed top & bottom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import Picture from "./components/Picture";
+import Dialog from "./components/Dialog";
 
-function App() {
+const App = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Handle form submission here
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid style={{ height: "100vh" }}>
+      <Row style={{ height: "30vh", backgroundColor: "#f8f9fa" }}>
+        <Col className="d-flex justify-content-center align-items-center">
+          <div className="fixed-top text-center py-3">
+            <Picture></Picture>
+          </div>
+        </Col>
+      </Row>
+      <Row style={{ height: "60vh", overflowY: "scroll" }}>
+        <Col className="d-flex justify-content-center align-items-center">
+          {/* Your scrollable content here */}
+          <div>
+            {Array.from({ length: 50 }, (_, i) => (
+              <div key={i} className="my-3">
+                Scrollable content
+                blablablablablablablablablablablablablablablablablablablablabla{" "}
+                {i + 1}
+              </div>
+            ))}
+          </div>
+        </Col>
+      </Row>
+      <Row style={{ height: "10%", backgroundColor: "#f8f9fa" }}>
+        <Col className="d-flex justify-content-center align-items-center">
+          <div className="fixed-bottom pb-4">
+            <Form onSubmit={handleSubmit}>
+              <Form.Group className="d-flex px-5">
+                <Form.Control
+                  type="text"
+                  placeholder="Enter your message"
+                  className="mr-2"
+                />
+                <Button type="submit">Send</Button>
+              </Form.Group>
+            </Form>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
-}
+};
 
 export default App;
---
+++ src/components/Picture.js
@@ -0,0 +1,15 @@
+import Hypatia from "../assets/hypatia.png";
+
+function Picture() {
+  return (
+    <img
+      src={Hypatia}
+      alt="Hypatia"
+      style={{
+        width: "26vh",
+      }}
+    />
+  );
+}
+
+export default Picture;
